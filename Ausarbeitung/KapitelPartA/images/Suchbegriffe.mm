8f39667042040750681557b2a8958376c55014b6
j3ssi <jessica.buehler@udo.edu>
2020-06-13 00:55:15 +0200

    zwischenstand

---
+++ Ausarbeitung/KapitelPartA/images/Suchbegriffe.mm
@@ -0,0 +1,129 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Suchbegriffe" LOCALIZED_STYLE_REF="defaultstyle.details" FOLDED="false" ID="ID_1680081610" CREATED="1589479852855" MODIFIED="1590520886036"><hook NAME="MapStyle" zoom="0.75">
+    <properties show_icon_for_attributes="true" fit_to_viewport="false" show_note_icons="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="7" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<edge STYLE="bezier"/>
+<node TEXT="accelerate training of deep neural network" LOCALIZED_STYLE_REF="defaultstyle.details" POSITION="left" ID="ID_1631099486" CREATED="1589492281643" MODIFIED="1590520870503" HGAP_QUANTITY="270.49999235570453 pt" VSHIFT_QUANTITY="-149.99999552965178 pt">
+<edge COLOR="#7c0000"/>
+<hook NAME="FreeNode"/>
+<node TEXT="Weight Normalization: A Simple Reparameterization to Accelerate Training of Deep Neural Networks" LOCALIZED_STYLE_REF="defaultstyle.details" ID="ID_1861126777" CREATED="1589492304169" MODIFIED="1590520856261" HGAP_QUANTITY="-9.249999307096004 pt" VSHIFT_QUANTITY="-89.24999734014281 pt">
+<hook NAME="FreeNode"/>
+</node>
+<node TEXT="Accelerated CNN Training Through Gradient Approximation" LOCALIZED_STYLE_REF="defaultstyle.details" ID="ID_194527079" CREATED="1589492526781" MODIFIED="1590520870502" HGAP_QUANTITY="123.49999673664578 pt" VSHIFT_QUANTITY="63.749998100102 pt">
+<hook NAME="FreeNode"/>
+</node>
+</node>
+<node TEXT="half precision training" LOCALIZED_STYLE_REF="defaultstyle.details" FOLDED="true" POSITION="left" ID="ID_1182265456" CREATED="1589489140025" MODIFIED="1590520875405" HGAP_QUANTITY="243.4999931603672 pt" VSHIFT_QUANTITY="15.749999530613437 pt">
+<edge COLOR="#00ffff"/>
+<hook NAME="FreeNode"/>
+<node TEXT="Mixed Precision Training" LOCALIZED_STYLE_REF="defaultstyle.details" ID="ID_1880146634" CREATED="1589490812943" MODIFIED="1590520875404" HGAP_QUANTITY="43.99999910593036 pt" VSHIFT_QUANTITY="-43.49999870359901 pt">
+<hook NAME="FreeNode"/>
+</node>
+</node>
+<node TEXT="use less training data convolutional neural network" LOCALIZED_STYLE_REF="defaultstyle.details" POSITION="right" ID="ID_1782961083" CREATED="1589492818703" MODIFIED="1590520850371" HGAP_QUANTITY="160.2499956414105 pt" VSHIFT_QUANTITY="-83.99999749660499 pt">
+<edge COLOR="#00007c"/>
+<hook NAME="FreeNode"/>
+<node TEXT="Deep Learning on Small Datasets without Pre-Training using Cosine Loss" LOCALIZED_STYLE_REF="defaultstyle.details" ID="ID_431607176" CREATED="1589492867069" MODIFIED="1590520850369" HGAP_QUANTITY="58.24999868124728 pt" VSHIFT_QUANTITY="-143.99999570846572 pt">
+<hook NAME="FreeNode"/>
+</node>
+<node TEXT="Learning Structure and Strength of CNN Filters for Small Sample Size Training" LOCALIZED_STYLE_REF="defaultstyle.details" ID="ID_1688713358" CREATED="1589492847957" MODIFIED="1590520673472" HGAP_QUANTITY="352.24998991936474 pt" VSHIFT_QUANTITY="-87.74999738484628 pt">
+<hook NAME="FreeNode"/>
+</node>
+</node>
+<node TEXT="fast training convolutional neural network" LOCALIZED_STYLE_REF="defaultstyle.details" POSITION="right" ID="ID_1536162942" CREATED="1589480467846" MODIFIED="1591030209691" HGAP_QUANTITY="-348.99998918175726 pt" VSHIFT_QUANTITY="89.2499973401428 pt">
+<font SIZE="16"/>
+<edge COLOR="#ff0000"/>
+<hook NAME="FreeNode"/>
+<node TEXT="PruneTrain: Fast Neural Network Training by Dynamic Sparse Model Reconfiguration" LOCALIZED_STYLE_REF="defaultstyle.details" ID="ID_824606526" CREATED="1589480536967" MODIFIED="1590520810125" HGAP_QUANTITY="-343.7499893382195 pt" VSHIFT_QUANTITY="-50.9999984800816 pt">
+<hook NAME="FreeNode"/>
+</node>
+<node TEXT="A Survey of Model Compression and Acceleration&#xa;for Deep Neural Networks" LOCALIZED_STYLE_REF="defaultstyle.details" ID="ID_227886083" CREATED="1589487117128" MODIFIED="1590520969239" HGAP_QUANTITY="-88.74999693781146 pt" VSHIFT_QUANTITY="64.49999807775026 pt">
+<hook NAME="FreeNode"/>
+<node TEXT="Net2net: Accelerating learning&#xa;via knowledge transfer" ID="ID_1468016218" CREATED="1589487136178" MODIFIED="1591030215922" HGAP_QUANTITY="80.74999801069504 pt" VSHIFT_QUANTITY="140.2499958202244 pt">
+<font SIZE="16"/>
+<node TEXT="MorphNet: Fast &amp; Simple Resource-Constrained Structure Learning of Deep Networks" ID="ID_1465928247" CREATED="1589491571681" MODIFIED="1590520972297" BACKGROUND_COLOR="#ffff00" HGAP_QUANTITY="-62.49999772012242 pt" VSHIFT_QUANTITY="76.49999772012241 pt">
+<font SIZE="14"/>
+</node>
+</node>
+<node TEXT="S3pool: Pooling with stochastic spatial sampling" ID="ID_822319272" CREATED="1589492606219" MODIFIED="1591030225068" HGAP_QUANTITY="-375.99998837709455 pt" VSHIFT_QUANTITY="-27.749999172985575 pt">
+<font SIZE="16"/>
+</node>
+</node>
+<node TEXT="Fast Training of Convolutional Neural Networks via Kernel Rescaling" LOCALIZED_STYLE_REF="defaultstyle.details" ID="ID_667295069" CREATED="1589480622699" MODIFIED="1590520951126" HGAP_QUANTITY="301.2499914392832 pt" VSHIFT_QUANTITY="75.74999774247414 pt">
+<hook NAME="FreeNode"/>
+<font SIZE="14"/>
+</node>
+</node>
+<node TEXT="faster gradient convolutional neural network" LOCALIZED_STYLE_REF="defaultstyle.details" POSITION="right" ID="ID_1908730828" CREATED="1589480520548" MODIFIED="1590520843764" HGAP_QUANTITY="216.4999939650299 pt" VSHIFT_QUANTITY="96.7499971166254 pt">
+<edge COLOR="#ff00ff"/>
+<hook NAME="FreeNode"/>
+<node TEXT="Accelerated CNN Training Through Gradient Approximation" LOCALIZED_STYLE_REF="defaultstyle.details" ID="ID_855940124" CREATED="1589491757137" MODIFIED="1590520843763" HGAP_QUANTITY="133.99999642372143 pt" VSHIFT_QUANTITY="-68.99999794363981 pt">
+<hook NAME="FreeNode"/>
+</node>
+<node TEXT="Accelerating Deep Neural Network Training with Inconsistent Stochastic Gradient Descent" LOCALIZED_STYLE_REF="defaultstyle.details" ID="ID_372161278" CREATED="1589491790617" MODIFIED="1590520836250" HGAP_QUANTITY="99.4999974519015 pt" VSHIFT_QUANTITY="80.99999758601197 pt">
+<hook NAME="FreeNode"/>
+</node>
+</node>
+</node>
+</map>
